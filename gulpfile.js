be4117105a6487e3ce4717388a64596d1cdfdb87
Jessita Coutinho <jsst_coutinho@yahoo.co.in>
2017-11-21 15:12:22 +1300

    using nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
 var inject = require('gulp-inject');
+var nodemon = require('gulp-nodemon');
 
 var jsFiles = ['*.js', 'src/**/*.js'];
 
@@ -24,12 +25,27 @@ gulp.task('inject', function () {
 
     var injectSrc = gulp.src(['./public/css/*.css', './public/js/*.js'], {
         read: false
-    })
+    });
     var injectOptions = {
         ignorePath: '/public'
-    }
+    };
     return gulp.src('./src/views/*.html')
         .pipe(wiredep(options))
         .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
-})
+});
+
+gulp.task('serve', ['style', 'inject'], function () {
+    var options = {
+        script: 'app.js',
+        delayTime: 1,
+        env: {
+            'PORT': 3000
+        },
+        watch: jsFiles
+    };
+    return nodemon(options)
+        .on('restart', function (ev) {
+            console.log('Restarting the server...');
+        });
+});
