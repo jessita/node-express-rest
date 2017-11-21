8cfb55bbf78f8285544037bc440b561741f22c85
Jessita Coutinho <jsst_coutinho@yahoo.co.in>
2017-11-21 15:43:15 +1300

    Jade template:
    1. creating a simple template with classes
    2. looping over list
    3. integrating with gulp-inject

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 
-app.use(express.static('src/views'));
+app.set('views', 'src/views');
+app.set('view engine', 'Jade');
 
 app.get('/', function (req, res) {
-    res.send('Hello world');
+    res.render('index', {
+        list: ['a', 'b']
+    });
 });
 
 app.get('/books', function (req, res) {
